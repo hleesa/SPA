42e886334218e3c75d7cb22f08dad7599863151a
lsh <salee2@student.42seoul.kr>
2024-01-30 18:11:52 +0900

    feat: no refresh

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,8 @@
+const navigateTo = url =>{
+    history.pushState(null, null, url);
+    router();
+}
+
 const router = async () => {
     const routes = [
         {path: "/", view: () => console.log("Viewing Dashboard")},
@@ -22,11 +27,15 @@ const router = async () => {
         };
     }
 
-    console.log(typeof routes);
-    console.log(typeof routes[0]);
     console.log(match.route.view());
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
     router();
 });
