f58a47183a8d7565b9d200062b4a96860745f763
lsh <salee2@student.42seoul.kr>
2024-01-31 14:39:07 +0900

    simple spa

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,6 @@
+import Dashboard from "./views/Dashboard.js";
+import Posts from "./views/Post.js";
+import Settings from "./views/Settings.js";
 const navigateTo = url =>{
     history.pushState(null, null, url);
     router();
@@ -5,9 +8,9 @@ const navigateTo = url =>{
 
 const router = async () => {
     const routes = [
-        {path: "/", view: () => console.log("Viewing Dashboard")},
-        {path: "/posts", view: () => console.log("Viewing Posts")},
-        {path: "/settings", view: () => console.log("Viewing Settings")},
+        {path: "/", view: Dashboard},
+        {path: "/posts", view: Posts},
+        {path: "/settings", view: Settings},
     ];
 
     // Test each  route for potential match
@@ -27,8 +30,10 @@ const router = async () => {
         };
     }
 
-    console.log(match.route.view());
-}
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+ }
 
 window.addEventListener("popstate", router);
 document.addEventListener("DOMContentLoaded", () => {
